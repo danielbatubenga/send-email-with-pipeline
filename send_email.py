86863d2dafe79ca8147e63e505f02e4df768693f
Daniel Batubenga <daniel.batubenga@sidea.com.br>
2024-07-07 23:49:42 +0100

    testando email receiver automatic

--- send_email.py
+++ send_email.py
@@ -2,21 +2,31 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
+import requests
+
+def get_commit_author_email(repo, commit_sha, github_token):
+    url = f"https://api.github.com/repos/{repo}/commits/{commit_sha}"
+    headers = {
+        "Authorization": f"token {github_token}"
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    commit_data = response.json()
+    email = commit_data['commit']['author']['email']
+    return email
 
 def send_email(subject, body, to_address, from_address, smtp_server, smtp_port, username, password):
-    # Cria a mensagem de email
     msg = MIMEMultipart()
     msg['From'] = from_address
     msg['To'] = to_address
     msg['Subject'] = subject
-
-    # Anexa o corpo do email
     msg.attach(MIMEText(body, 'html'))
 
-    # Conecta ao servidor SMTP e envia o email
     try:
         server = smtplib.SMTP(smtp_server, smtp_port)
+        server.ehlo()
         server.starttls()
+        server.ehlo()
         server.login(username, password)
         server.sendmail(from_address, to_address, msg.as_string())
         server.quit()
@@ -25,13 +35,18 @@ def send_email(subject, body, to_address, from_address, smtp_server, smtp_port, 
         print(f"Erro ao enviar email: {e}")
 
 if __name__ == "__main__":
+    repo = os.getenv('GITHUB_REPOSITORY')
+    commit_sha = os.getenv('GITHUB_SHA')
+    github_token = os.getenv('GITHUB_TOKEN')
+
     subject = "Assunto do Email"
     body = "Corpo do email. Pode incluir <b>HTML</b> se necessário."
-    to_address = os.getenv('TO_EMAIL')
     from_address = os.getenv('FROM_EMAIL')
     smtp_server = os.getenv('SMTP_SERVER')
     smtp_port = os.getenv('SMTP_PORT')
     username = os.getenv('SMTP_USERNAME')
     password = os.getenv('SMTP_PASSWORD')
+
+    to_address = get_commit_author_email(repo, commit_sha, github_token)
 
     send_email(subject, body, to_address, from_address, smtp_server, smtp_port, username, password)
